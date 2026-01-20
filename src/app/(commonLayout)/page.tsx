4bce908669cf39a49b4606850ec851e3a0a2e150
Afnan Sayed <afnansayed1973@gmail.com>
2026-01-20 18:40:06 +0600

    create a next project with shadcn

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold">Home page</h1>
+      <Button>Home</Button>
+    </div>
+  );
+}
